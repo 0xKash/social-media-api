f644b9204459d202014253ee38c7f0be9a4f2f3f
kash <wwwkash2@gmail.com>
2025-07-24 15:28:04 +0200

    feat: first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+// imports
+const express = require("express");
+const cors = require("cors");
+
+// app setup
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.set(express.urlencoded({ extended: true }));
+
+// errrorHandler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).send(err);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (req, res) => {
+  console.log(`Listening on PORT: ${port}`);
+});
