4c04d90c9778d593baaaefed1d3138b1e7d0d5a4
kash <wwwkash2@gmail.com>
2025-07-26 18:49:10 +0200

    feat: add omitConfig to prisma & connect postControllers to endpoints

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,18 @@
 // imports
 const { Router } = require("express");
 const { isAuth } = require("../lib/authMiddleware");
+const {
+  createPost,
+  getAllPosts,
+  getPostById,
+} = require("../controllers/postController");
 
 // setup
 const postRouter = Router();
 
-postRouter.get("/", isAuth /* getAllPosts */);
-postRouter.post("/", isAuth /* createPost */);
+postRouter.get("/", isAuth, getAllPosts);
+postRouter.post("/", isAuth, createPost);
 
-postRouter.get("/:postId", isAuth /*getPostById */);
+postRouter.get("/:postId", isAuth, getPostById);
 
 module.exports = postRouter;
